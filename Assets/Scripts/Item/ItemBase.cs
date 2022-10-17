823b718eee318b06c69a81618dc77f4a48df9c57
yzy <yzysonic@gmail.com>
2022-10-18 03:32:54 +0900

    【PG】 新增獲得道具時的效果

--- Assets/Scripts/Item/ItemBase.cs
+++ Assets/Scripts/Item/ItemBase.cs
@@ -6,12 +6,22 @@ namespace NSS
 {
     public class ItemBase : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject getEffect;
+
         public virtual void ApplyToCharacter(Character character)
         {
             if (ItemManager.IsCreated)
             {
                 ItemManager.Instance.OnItemApplied(this);
             }
+
+            if (getEffect)
+            {
+                GameObject effect = Instantiate(getEffect, transform.position + Vector3.back, Quaternion.identity);
+                Destroy(effect, 1.0f);
+            }
+
             Destroy(gameObject);
         }
     }
